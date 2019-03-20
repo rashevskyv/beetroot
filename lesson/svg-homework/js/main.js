2c1b2836776391d315ca1a3031b88687ba56ffbf
rashevskyv <rashevsky.v@gmail.com>
2019-03-20 10:01:56 +0200

    lessons

---
+++ lesson/svg-homework/js/main.js
@@ -0,0 +1,11 @@
+// initialize fullpage.js
+
+new fullpage('#fullpage', {
+    //options here
+    autoScrolling:true,
+    // scrollHorizontally: true,
+    scrollOverflow: true
+});
+
+//methods
+fullpage_api.setAllowScrolling(true);
